5abdd2f24a71f4b7b7df20ad6e4826867b0a5888
Patrick Broens <patrick@patrickbroens.nl>
2014-01-30 14:16:15 +0100

    [TASK] Reinstate the FlexForm for CE "table"
    The FlexForm for the content element "Table" is missing.
    
    This patch fixes this by adding a copy of the FlexForm from css_styled_content, but in a more structured way of storing files.

--- ext_tables.php
+++ ext_tables.php
@@ -9,3 +9,15 @@ if (!defined('TYPO3_MODE')) {
 	'Configuration/TypoScript',
 	'Content Elements'
 );
+
+	// Add flexform for CE "table"
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+	'*',
+	'FILE:EXT:contentelements/Configuration/FlexForm/Table.xml',
+	'table'
+);
+
+	// Add the flexform to TCA for CE "table"
+$GLOBALS['TCA']['tt_content']['types']['table']['showitem'] = 'CType;;4;;1-1-1, hidden, header;;3;;2-2-2, linkToTop;;;;4-4-4,
+			--div--;LLL:EXT:cms/locallang_ttc.xlf:CType.I.5, layout;;10;;3-3-3, cols, bodytext;;9;nowrap:wizards[table], text_properties, pi_flexform,
+			--div--;LLL:EXT:cms/locallang_tca.xlf:pages.tabs.access, starttime, endtime, fe_group';
