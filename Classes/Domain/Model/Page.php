73d4c194f7e7fc481dcdc755eb8fa1c182ec0037
Patrick Broens <patrick@patrickbroens.nl>
2014-02-02 17:35:25 +0100

    [TASK] Remove unused variables from page model
    The page model contains some unused variables, which can be removed
    
    This patch removes these variables

--- Classes/Domain/Model/Page.php
+++ Classes/Domain/Model/Page.php
@@ -31,14 +31,6 @@ namespace PatrickBroens\Contentelements\Domain\Model;
 class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 
 	/**
-	 * The parent of the page
-	 *
-	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\PatrickBroens\Contentelements\Domain\Model\Page>
-	 * @lazy
-	 */
-	protected $parent;
-
-	/**
 	 * The page title
 	 *
 	 * @var string
@@ -81,14 +73,6 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 	protected $description;
 
 	/**
-	 * The page language overlay
-	 *
-	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\PatrickBroens\Contentelements\Domain\Model\PageLanguageOverlay>
-	 * @lazy
-	 */
-	protected $overlay;
-
-	/**
 	 * The categories of the page
 	 *
 	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category>
@@ -97,14 +81,8 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 	protected $categories;
 
 	/**
-	 * The page content
+	 * Getter for the page title
 	 *
-	 * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\PatrickBroens\Contentelements\Domain\Model\Content>
-	 * @lazy
-	 */
-	protected $content;
-
-	/**
 	 * @return mixed
 	 */
 	public function getTitle() {
@@ -112,6 +90,8 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 	}
 
 	/**
+	 * Getter for the navigation title
+	 *
 	 * @return mixed
 	 */
 	public function getNavigationTitle() {
@@ -126,6 +106,7 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 	}
 
 	/**
+	 * Getter for the page abstract
 	 * @return mixed
 	 */
 	public function getAbstract() {
@@ -133,6 +114,8 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 	}
 
 	/**
+	 * Getter for the keywords
+	 *
 	 * @return mixed
 	 */
 	public function getKeywords() {
@@ -140,57 +123,11 @@ class Page extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
 	}
 
 	/**
+	 * Getter for the page description
+	 *
 	 * @return mixed
 	 */
 	public function getDescription() {
 		return $this->description;
-	}
-
-	/**
-	 * Setter for content
-	 *
-	 * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage $content One or more content objects
-	 * @return void
-	 */
-	public function setContent(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $content) {
-		$this->content = $content;
-	}
-
-	/**
-	 * Adds content to this page
-	 *
-	 * @param \PatrickBroens\Contentelements\Domain\Model\Content $content
-	 * @return void
-	 */
-	public function addCategory(\PatrickBroens\Contentelements\Domain\Model\Content $content) {
-		$this->content->attach($content);
-	}
-
-	/**
-	 * Removes content from this page
-	 *
-	 * @param \PatrickBroens\Contentelements\Domain\Model\Content $content
-	 * @return void
-	 */
-	public function removeCategory(\PatrickBroens\Contentelements\Domain\Model\Content $content) {
-		$this->content->detach($content);
-	}
-
-	/**
-	 * Remove all content from this page
-	 *
-	 * @return void
-	 */
-	public function removeAllContent() {
-		$this->content = new \TYPO3\CMS\Extbase\Persistence\ObjectStorage();
-	}
-
-	/**
-	 * Getter for content
-	 *
-	 * @return \TYPO3\CMS\Extbase\Persistence\ObjectStorage A storage holding \PatrickBroens\Contentelements\Domain\Model\Content objects
-	 */
-	public function getContent() {
-		return $this->content;
 	}
 }
